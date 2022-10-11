396358969884e363a81feb16bca33d34fb887307
xpanvictor <xpanvictor@gmail.com>
2022-10-11 14:47:15 +0100

    feat: basic bencode parsing

---
+++ build/index.js
@@ -0,0 +1,5 @@
+'use strict';
+const fs = require('fs');
+const bencode = require('bencode');
+const torrent = bencode.decode(fs.readFileSync(__dirname + '/dsa.torrent'));
+console.log(torrent.announce.toString('utf8'));
